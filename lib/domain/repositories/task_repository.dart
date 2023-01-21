c8e71814bd027b2d63b372102c4ca0566e9989fb
Vishal Makasana <vishal.makasana@hotmail.com>
2023-01-21 18:50:18 +0530

    local repository test cases added code refactoring

--- lib/domain/repositories/task_repository.dart
+++ lib/domain/repositories/task_repository.dart
@@ -3,9 +3,11 @@ import 'package:ipotato_timer/domain/entities/task_entity.dart';
 abstract class TaskRepository {
   Future<int> addTask(TaskEntity taskEntity);
 
-  Future<bool> deleteTask(int taskId);
+  Future<TaskEntity?> getTask(int taskId);
 
   Future<List<TaskEntity>> getAllTask();
+
+  Future<bool> deleteTask(int taskId);
 
   Future<bool> pauseTask(DateTime pausedTime, int taskId);
 
